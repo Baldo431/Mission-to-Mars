0bc613ab62531526b1f4ed70c2f00586d5f785f8
Baldo431 <s.alvarado6610@live.com>
2022-09-20 23:53:53 -0600

    Adding module challenge files and changes

--- scraping.py
+++ scraping.py
@@ -15,6 +15,7 @@ def scrape_all():
     news_title, news_paragraph = mars_news(browser)
     images = featured_image(browser)
     facts = mars_facts()
+    hemispheres = mars_hemispheres(browser)
 
     # Store results in dictionary
     data = {
@@ -22,6 +23,7 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": images,
         "facts": facts,
+        "hemisphere_images": hemispheres,
         "last_modified": dt.datetime.now()
     }
 
@@ -105,7 +107,57 @@ def mars_facts():
     df = df[1:]
     df.set_index(['Mars - Earth Comparison', 'Mars', 'Earth'], inplace=True)
     
-    return df.to_html()
+    return df.to_html(classes='dataframe table-responsive table-striped table-hover')
+
+
+
+def mars_hemispheres(browser):
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+
+    # Convert the base url visited to html
+    hemisphere_soup = soup(browser.html, 'html.parser')
+
+    #Search for the objects that contain the information for each hemisphere
+    try:
+        links_soup = hemisphere_soup.find('div', class_='collapsible results')
+        links_soup = links_soup.find_all('div', class_='description')
+    except AttributeError:
+        return None
+
+    #Iterate through the hemispheres
+    for item in links_soup:
+        
+        try:
+            #Pull the url where the full image is located.
+            url_to_visit = item.find('a', class_='itemLink product-item').get('href')
+            #Pull the title for each hemisphere.
+            title = item.find('h3').get_text()
+        except AttributeError:
+            return None
+
+        browser.visit(f'{url}{url_to_visit}')
+        
+        # Search for the link to the full sized image
+        html = browser.html
+        image_soup = soup(html, 'html.parser')
+
+        try:
+            image_url = image_soup.find('a', string='Sample').get('href')
+        except AttributeError:
+            return None        
+
+        #Add the info as a dictionary to the list
+        hemisphere_image_urls.append({'img_url' : f'{url}{image_url}', 'title': title})
+
+    # 4. Return a list that holds the dictionary of each image url and title.
+    return hemisphere_image_urls
 
 
 if __name__ == "__main__":
